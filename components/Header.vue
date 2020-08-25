b7b9ab791348361c0bad4dc479ed905fd15538e5
Max <m.blacher@web.de>
2020-08-26 01:08:08 +0200

    babysteps

---
+++ components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <ul>
+      <li>
+        <nuxt-link to="/">Home</nuxt-link>
+        </li>
+        <li>
+        <nuxt-link to="/about">About</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ pages/about/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h2>This is a about page</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+    title: "About"
+    }
+  }
+}
+</script>
