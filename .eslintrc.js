d5d088fb09bde266afcf4d9a772b13290c0d0612
Max <m.blacher@web.de>
2020-09-06 18:43:20 +0200

    Updated linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,24 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
     node: true,
+    browser: true,
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+  globals: {
+    $nuxt: true,
   },
   parserOptions: {
     parser: 'babel-eslint',
   },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended',
-    'plugin:prettier/recommended',
-  ],
-  // add your custom rules here
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-};
+}
