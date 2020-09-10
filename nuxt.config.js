045a700d5b869026e6735ddc982b24c5269c8927
Max <m.blacher@web.de>
2020-09-10 15:43:14 +0200

    Opengraph icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,34 +34,34 @@ export default {
       },
       {
         property: 'og:title',
-        content: 'Surf-Bretagne',
+        content: 'A forecast for the beaches around Brittany',
       },
       {
         property: 'og:image',
-        content: '~/static/SBScreenshot.png',
+        content: 'SBScreenshot.png',
       },
     ],
     link: [
       {
         rel: 'apple-touch-icon',
         sizes: '180x180',
-        href: '~/assets/apple-touch-icon.png',
+        href: 'apple-touch-icon.png',
       },
       {
         rel: 'icon',
         type: 'image/png',
         sizes: '32x32',
-        href: '~/assets/favicon-32x32.png',
+        href: 'favicon-32x32.png',
       },
       {
         rel: 'icon',
         type: 'image/png',
         sizes: '16x16',
-        href: '~/assets/favicon-16x16.png',
+        href: 'favicon-16x16.png',
       },
       {
         rel: 'manifest',
-        href: '~/assets/site.webmanifest',
+        href: 'site.webmanifest',
       },
     ],
   },
